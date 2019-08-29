5f92d712239a0f5f13e5b01c790bd48e8a05e72a
azattspace97@gmail.com <park!0!512>
2019-08-29 15:28:36 +0900

    Empty Android Project

---
+++ app/src/main/java/com/graduation/alarmsync/MainActivity.java
@@ -0,0 +1,14 @@
+package com.graduation.alarmsync;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
