826dc3ea6188964a89d5fbaa9b5c0a488cf43411
azattspace97@gmail.com <park!0!512>
2019-09-08 23:44:06 +0900

    19/09/08 End

--- app/src/main/java/com/graduation/alarmsync/MainActivity.java
+++ app/src/main/java/com/graduation/alarmsync/MainActivity.java
@@ -1,8 +1,12 @@
 package com.graduation.alarmsync;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // getSupportActionBar().setTitle("ACTIONBAR");
+        // getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF339999));
+        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.mainLayout);
+        final View drawerView = (View) findViewById(R.id.drawer);
+        Button btnOpenDrawer = (Button) findViewById(R.id.testbtn);
+        btnOpenDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
 }
