795f0260d9ad4c3bdfc5e23b4b0cc681cf26227a
azattspace97@gmail.com <park!0!512>
2019-09-22 19:34:23 +0900

    19/09/22 dataBinding 적용

--- app/src/main/java/com/graduation/alarmsync/LoginActivity.java
+++ app/src/main/java/com/graduation/alarmsync/LoginActivity.java
@@ -2,11 +2,22 @@ package com.graduation.alarmsync;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.databinding.DataBindingUtil;
+
+import com.graduation.alarmsync.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        ActivityLoginBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        binding.signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/graduation/alarmsync/MainActivity.java
+++ app/src/main/java/com/graduation/alarmsync/MainActivity.java
@@ -1,6 +1,7 @@
 package com.graduation.alarmsync;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
@@ -9,19 +10,18 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
+import com.graduation.alarmsync.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         // getSupportActionBar().setTitle("ACTIONBAR");
         // getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF339999));
         // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        final Button btnMainMenuInfo = findViewById(R.id.main_menu_info);
-
-        btnMainMenuInfo.setOnClickListener(new View.OnClickListener() {
+        binding.mainMenuInfo.setOnClickListener(new View.OnClickListener() {
             // 로그인이 되어있는지 체크하고 로그인 페이지를 실행할것
             @Override
             public void onClick(View v) {
@@ -30,13 +30,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.mainLayout);
-        final View drawerView = (View) findViewById(R.id.drawer);
-        Button btnOpenDrawer = (Button) findViewById(R.id.testbtn);
-        btnOpenDrawer.setOnClickListener(new View.OnClickListener() {
+        binding.testbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                drawerLayout.openDrawer(drawerView);
+                binding.mainLayout.openDrawer(binding.drawer);
             }
         });
     }
