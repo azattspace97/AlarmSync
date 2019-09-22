c0a5e8fd37ad70f1c09bb7dc6790186a83d6b5ae
azattspace97@gmail.com <park!0!512>
2019-09-22 15:09:20 +0900

    19/09/22 Menu update2

--- app/src/main/java/com/graduation/alarmsync/MainActivity.java
+++ app/src/main/java/com/graduation/alarmsync/MainActivity.java
@@ -3,13 +3,13 @@ package com.graduation.alarmsync;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
         // getSupportActionBar().setTitle("ACTIONBAR");
         // getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF339999));
         // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final Button btnMainMenuInfo = findViewById(R.id.main_menu_info);
+
+        btnMainMenuInfo.setOnClickListener(new View.OnClickListener() {
+            // 로그인이 되어있는지 체크하고 로그인 페이지를 실행할것
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
         final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.mainLayout);
         final View drawerView = (View) findViewById(R.id.drawer);
