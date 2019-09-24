49ed2292f9b54b9f5db4997b04021908757da5b2
azattspace97@gmail.com <park!0!512>
2019-09-24 21:40:30 +0900

    09/24 Login, Sign창 추가

--- app/src/main/java/com/graduation/alarmsync/MainActivity.java
+++ app/src/main/java/com/graduation/alarmsync/MainActivity.java
@@ -2,15 +2,10 @@ package com.graduation.alarmsync;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
