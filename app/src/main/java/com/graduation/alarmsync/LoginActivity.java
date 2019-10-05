be6be5f1726f12a3ba4f7b37f5d20f8bc9b3a89e
azattspace97@gmail.com <park!0!512>
2019-10-05 18:38:27 +0900

    10/05 Login 성공

--- app/src/main/java/com/graduation/alarmsync/LoginActivity.java
+++ app/src/main/java/com/graduation/alarmsync/LoginActivity.java
@@ -3,10 +3,13 @@ package com.graduation.alarmsync;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.databinding.DataBindingUtil;
 
+import com.graduation.alarmsync.connadmin.LoginSignTask;
 import com.graduation.alarmsync.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends Activity {
@@ -14,11 +17,39 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final ActivityLoginBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        binding.etpwd.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
         binding.btnSign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent sintent = new Intent(LoginActivity.this, SignActivity.class);
                 startActivity(sintent);
+            }
+        });
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = binding.etid.getText().toString();
+                String pwd = binding.etpwd.getText().toString();
+
+                try {
+                    String result  = new LoginSignTask().execute(id,pwd,"login").get();
+                    if(result.equals("ok")) {
+                        Toast.makeText(LoginActivity.this,"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else if(result.equals("false")) {
+                        Toast.makeText(LoginActivity.this,"아이디 또는 비밀번호가 틀리거나 존재하지 않습니다.",Toast.LENGTH_SHORT).show();
+                        binding.etpwd.setText("");
+                    } else if(result.equals("false2")) {
+                        Toast.makeText(LoginActivity.this,"아이디 또는 비밀번호가 틀리거나 존재하지 않습니다.",Toast.LENGTH_SHORT).show();
+                        binding.etpwd.setText("");
+                    } else {
+                        Toast.makeText(LoginActivity.this, "네트워크에 문제가 생기거나 사이트가 접속이 되지 않습니다.",Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {}
             }
         });
     }
--- app/src/main/java/com/graduation/alarmsync/SignActivity.java
+++ app/src/main/java/com/graduation/alarmsync/SignActivity.java
@@ -1,8 +1,13 @@
 package com.graduation.alarmsync;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.databinding.DataBindingUtil;
 
@@ -13,6 +18,39 @@ public class SignActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final ActivitySignBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_sign);
+
+        binding.etpwd.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        binding.etpwdcheck.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+        binding.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = binding.etid.getText().toString();
+                String pwd = binding.etpwd.getText().toString();
+            }
+        });
+
+        TextWatcher pwdChecking = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void afterTextChanged(Editable editable) { }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String p1 = binding.etpwd.getText().toString();
+                String p2 = binding.etpwdcheck.getText().toString();
+
+                if(p1.equals(p2)) {
+                    binding.etpwd.setBackgroundResource(R.drawable.sign_edit_true);
+                    binding.etpwdcheck.setBackgroundResource(R.drawable.sign_edit_true);
+                } else {
+                    binding.etpwd.setBackgroundResource(R.drawable.sign_edit_false);
+                    binding.etpwdcheck.setBackgroundResource(R.drawable.sign_edit_false);
+                }
+            }
+        };
+        binding.etpwdcheck.addTextChangedListener(pwdChecking);
+        binding.etpwd.addTextChangedListener(pwdChecking);
 
         binding.btnReturn.setOnClickListener(new View.OnClickListener() {
             @Override
